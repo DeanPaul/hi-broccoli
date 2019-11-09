d1e1ebf51c8d82889957b07af3027c1da99f56bf
DeanPaul <83623378@qq.com>
2019-11-09 23:19:45 +0800

    add key for invite input and div

--- src/components/invite/InviteModalContent.js
+++ src/components/invite/InviteModalContent.js
@@ -7,9 +7,9 @@ export default function InviteModalContent(props) {
         <span>{title}</span>
         <div className='invite-modal-content'>
             {
-                content.inputs && content.inputs.map(input => (
-                    <div className={'invite-input'}>
-                        <input name={input.name} onBlur={input.save} className={input.className}/>
+                content.inputs && content.inputs.map((input) => (
+                    <div key={'invite-div'+ input.name} className={'invite-input'}>
+                        <input key={'invite-input'+ input.name} name={input.name} onBlur={input.save} className={input.className}/>
                     </div>))
             }
             {content.content_text && <span>{content.content_text}</span>}
